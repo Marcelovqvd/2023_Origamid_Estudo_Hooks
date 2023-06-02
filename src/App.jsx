84cc2d1e317a66f38384a4ce6cd4977113cfa735
Marcelo <marcelovqvd@gmail.com>
2023-06-01 23:22:48 -0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import { useState } from 'react'
-import { Main } from './components/main'
+import React from 'react';
+import Produto from './Produto';
 
-function App() {
+const App = () => {
+  const [produto, setProduto] = React.useState(null);
+
+  React.useEffect(() => {
+    const produtoLocal = window.localStorage.getItem('produto');
+    if (produtoLocal !== 'null') setProduto(produtoLocal);
+  }, []);
+
+  React.useEffect(() => {
+    if (produto !== null) window.localStorage.setItem('produto', produto);
+  }, [produto]);
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
+
   return (
-    <Main />
-  )
-}
+    <div>
+      <h1>Preferência: {produto}</h1>
+      <button style={{ marginRight: '1rem' }} onClick={handleClick}>
+        notebook
+      </button>
+      <button onClick={handleClick}>smartphone</button>
+      <Produto produto={produto} />
+    </div>
+  );
+};
 
-export default App
+export default App;
